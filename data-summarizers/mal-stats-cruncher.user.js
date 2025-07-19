816f9bd914a25273d7b425dc01eaae33ad2c9449
SlimRunner <dv.polygon@gmail.com>
2025-07-19 16:54:37 -0700

    Add new script to log additional stats in MAL

---
+++ data-summarizers/mal-stats-cruncher.user.js
@@ -0,0 +1,85 @@
+// ==UserScript==
+// @name        mal-stat-summarizer
+// @namespace   slidav.gradescope
+// @version     0.0.1
+// @author      SlimRunner
+// @description Computes useful ratios out of an entry stats.
+// @grant       none
+// @match       https://myanimelist.net/anime/*/stats
+// @downloadURL https://github.com/SlimRunner/userscript-collection/raw/main/data-summarizers/mal-stats-cruncher.user.js
+// @updateURL   https://github.com/SlimRunner/userscript-collection/raw/main/data-summarizers/mal-stats-cruncher.user.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  // --
+  const summary = getSummary();
+  const ratings = getRatings();
+
+  const ratingsTotal = ratings.reduce((x, [r, v]) => x + v, 0);
+  const ratingsMean = ratings.reduce((x, [r, v]) => x + r * v / ratingsTotal, 0);
+  const ratingsStDev = Math.sqrt(ratings.reduce((x, [r, v]) => x + (r - ratingsMean) ** 2 * v / ratingsTotal, 0));
+
+  const dropRate = summary.dropped / (summary.dropped + summary.completed + summary.watching);
+  const completionRate = summary.completed / (summary.dropped + summary.completed + summary.watching + summary.onHold);
+  const engagementRate = (summary.watching + summary.completed) / summary.total;
+  const abandonmentRate = (summary.dropped + summary.onHold) / (summary.dropped + summary.completed + summary.watching + summary.onHold);
+
+  console.table({
+    ratingsTotal,
+    ratingsMean,
+    ratingsStDev,
+  });
+
+  console.table({
+    dropRate: Math.round(10000 * dropRate) / 100,
+    completionRate: Math.round(10000 * completionRate) / 100,
+    engagementRate: Math.round(10000 * engagementRate) / 100,
+    abandonmentRate: Math.round(10000 * abandonmentRate) / 100,
+  });
+
+  window.malStats = {
+    summary,
+    ratings
+  }
+
+  function getSummary() {
+    const elements = Array.from(document.querySelectorAll("#summary_stats~div"));
+    let result = null;
+
+    if (elements.length >= 6) {
+      elements.splice(6);
+      // broken down into multiple maps to improve readability
+      const rawNumbers = elements
+        .map(e => e.textContent.replace(",", ""))
+        .map(e => e.match(/\d+/).at(0))
+        .map(e => parseInt(e));
+      result = {
+        watching: rawNumbers[0],
+        completed: rawNumbers[1],
+        onHold: rawNumbers[2],
+        dropped: rawNumbers[3],
+        planToWatch: rawNumbers[4],
+        total: rawNumbers[5],
+      }
+    }
+
+    return result;
+  }
+
+  function getRatings() {
+    const elements = Array.from(document.querySelectorAll("#score_stats+.score-stats tr"));
+    let result = null;
+
+    if (elements.length === 10) {
+      // broken down into multiple maps to improve readability
+      const rawNumbers = elements
+        .map(e => e.textContent.trim())
+        .map(e => e.match(/(?<=\()\s*\d+\s*(?=votes\))/).at(0))
+        .map((e, i) => [10 - i, parseInt(e)]);
+      result = rawNumbers;
+    }
+
+    return result;
+  }
+})();
