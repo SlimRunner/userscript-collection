08b5f1cbf3ea9329e50b7a1391424089a5ea003a
SlimRunner <dv.polygon@gmail.com>
2026-06-30 13:07:36 -0700

    Add new script for wanikani

---
+++ ui-injection/wanikani-hide-eng.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name        WanikaniENGBlur
+// @namespace   slidav.Scripting
+// @version     0.0.1
+// @author      SlimRunner (David Flores)
+// @description Hides English translations in Wanikani
+// @grant       none
+// @match       https://www.wanikani.com/subject-lessons/*
+// @downloadURL https://github.com/SlimRunner/userscript-collection/raw/main/ui-injection/wanikani-hide-eng.user.js
+// @updateURL   https://github.com/SlimRunner/userscript-collection/raw/main/ui-injection/wanikani-hide-eng.user.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  addStyleSheet(
+    `\
+    .context-sentences .wk-text:not([lang]) {
+      filter: blur(5px);
+    }
+
+    .context-sentences .wk-text:hover {
+      filter: blur(0);
+    }
+    `,
+    true,
+  );
+
+  function addStyleSheet(rules, dedent = false) {
+    if (dedent) {
+      const TAB = /^\t/.test(rules) ? "\t" : " ";
+      const tabBase = rules
+        .match(new RegExp(String.raw`^${TAB}+(?!\n$)`, "gm"))
+        .reduce((acc, curr) => Math.min(acc, curr.length), Infinity);
+      let tab = "";
+      for (let i = 0; i < tabBase; ++i) {
+        tab += " ";
+      }
+      rules = rules.replaceAll(new RegExp(`^${TAB}{${tabBase}}`, "gm"), "");
+    }
+    const style = document.createElement("style");
+    style.textContent = rules;
+    document.head.append(style);
+    return style;
+  }
+})();
